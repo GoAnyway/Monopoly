d04525b148c81c75af2f84ac8b900a97598a72ed
GoAnyway <49254816+GoAnyway@users.noreply.github.com>
2020-11-23 11:31:23 +0300

    Update StringExtensions.cs

--- Utils/StringExtensions.cs
+++ Utils/StringExtensions.cs
@@ -7,24 +7,10 @@ namespace Utils
     {
         public static string Md5(this string input)
         {
-<<<<<<< HEAD
             using var md5 = MD5.Create();
             var inputBytes = Encoding.ASCII.GetBytes(input);
             var hashBytes = md5.ComputeHash(inputBytes);
             var sb = new StringBuilder();
-=======
-            using (var md5 = MD5.Create())
-            {
-                var inputBytes = Encoding.ASCII.GetBytes(input);
-                var hashBytes = md5.ComputeHash(inputBytes);
-                var sb = new StringBuilder();
-
-                sb.Append(hashBytes.Select(_ => _.ToString("X2")));
-                foreach (var hashByte in hashBytes)
-                {
-                    sb.Append(hashByte.ToString("X2"));
-                }
->>>>>>> f0c2a7537f1f47e30b52364019e11d70eb28d9d9
 
             foreach (var hashByte in hashBytes)
             {
